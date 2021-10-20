b3dac8f971df147cc951d39e9f49b3357a87d098
nashjunior <nashjuniorjj@gmail.com>
2021-10-20 20:55:36 -0300

    refactor: fix import

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,7 +1,7 @@
-from rest_framework.viewsets import ViewSet
+from rest_framework.viewsets import ModelViewSet
 from core.api.serializers import PontoTuristicoSerializer
 from core.models import PontoTuristico
 
-class PontoTuristicoViewSet (ViewSet):
+class PontoTuristicoViewSet (ModelViewSet):
     queryset = PontoTuristico.objects.all()
     serializer_class = PontoTuristicoSerializer
