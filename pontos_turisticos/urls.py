6935367f1a86663c95abd5246af65a26b320d99c
nashjunior <nashjuniorjj@gmail.com>
2021-10-21 18:51:37 -0300

    refactor: add enderecos route

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -19,10 +19,12 @@ from django.urls import path
 from rest_framework import routers
 from core.api.viewsets import PontoTuristicoViewSet
 from atracoes.api.viewsets import AtracoesViewSet
+from enderecos.api.viewsets import EnderecosViewSet
 
 router = routers.DefaultRouter()
 router.register(r'pontos_turisticos', PontoTuristicoViewSet)
 router.register(r'atracoes', AtracoesViewSet)
+router.register(r'enderecos', EnderecosViewSet)
 
 
 urlpatterns = [
