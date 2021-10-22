14d428666385b460931b2ad5db1d35430c25faf2
nashjunior <nashjuniorjj@gmail.com>
2021-10-22 15:54:48 -0300

    refactor: should handle manually requests view set

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,7 +1,26 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets, response
 from core.api.serializers import PontoTuristicoSerializer
 from core.models import PontoTuristico
 
-class PontoTuristicoViewSet (ModelViewSet):
+
+class PontoTuristicoViewSet (viewsets.ModelViewSet):
     queryset = PontoTuristico.objects.all()
     serializer_class = PontoTuristicoSerializer
+    
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+    
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+    
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+    
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+    
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
+    
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
